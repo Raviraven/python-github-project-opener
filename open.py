88dab424d68fd861608d29031e26f1222b578238
Michal Kruszewski <kruszewski2@gmail.com>
2020-03-30 11:22:40 +0200

    creating github url implemented

--- open.py
+++ open.py
@@ -1,5 +1,24 @@
+import sys
+import os
+import settings
+
 def open_project():
-    pass
+    project_name = get_project_name()
+    if not project_name:
+        project_name = 'test none'
+        current_directory = os.getcwd().split('\\')
+        project_name = '{0}-{1}'.format(current_directory[-2].lower(), current_directory[-1])
+    
+    url = 'https://github.com/{0}/{1}'.format(str(settings.github_username), project_name)
+    
+
+def get_project_name():
+    try:
+        project_name = str(sys.argv[1])
+        return project_name
+    except:
+        return None
+
 
 if __name__ == '__main__':
     open_project()
